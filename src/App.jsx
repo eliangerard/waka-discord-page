111801df4d443a0013bcc985a0dd4e6444e367b7
Elian <eliangerardiso@gmail.com>
2023-06-18 00:09:48 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Card } from './Components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `url(./src/assets/background.png)`,
+          height: "100vh",
+          width: "100vw",
+          zIndex: -1,
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#def0d6AA",
+            backdropFilter: "blur(100px)",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <Card/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
